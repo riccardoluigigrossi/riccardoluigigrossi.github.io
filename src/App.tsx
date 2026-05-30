b374a44a66ce295fb807a9d4ab7025e4de8c50fd
Riccardo <128899844+riccardoluigigrossi@users.noreply.github.com>
2026-05-30 09:03:39 +0200

    Update App.tsx
    Again an update on the copy of the destruction button...

--- src/App.tsx
+++ src/App.tsx
@@ -411,7 +411,7 @@ function Home() {
             style={{ cursor: 'pointer', userSelect: 'none' }}
             className={destroy === 'idle' ? 'underline' : undefined}
           >
-            {destroy === 'idle' ? "Leave instead" : 'Closing'}
+            {destroy === 'idle' ? "Leave no trace" : ''}
           </span>
         </p>
       </Section>
