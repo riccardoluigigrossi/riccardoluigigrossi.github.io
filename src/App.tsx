5728d3b1ae6bd47997e2c8b95e8e497c03455cff
Riccardo <128899844+riccardoluigigrossi@users.noreply.github.com>
2026-06-04 08:58:27 +0200

    Update reminder text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -476,9 +476,9 @@ function Home() {
                   </a>
                 </td>
                 <td>
-                  Reminder that feeling lost is often part of finding
+                  Reminder that feeling lost is often part of
                   <br />
-                  the way, isn’t it?
+                  getting somewhere meaningful
                 </td>
               </tr>
             </tbody>
